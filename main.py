cada8bf48f338a6cb7daba259fa673f4de227f9a
Anders Lauridsen <anders.hl.lauridsen@gmail.com>
2023-08-31 16:38:48 +0200

    Cleanup and created plotting notebook

--- main.py
+++ main.py
@@ -7,22 +7,9 @@ import pandas as pd
 import os
 
 
-def plot_register_distribution(data: pd.DataFrame) -> None:
-    fig, ax = plt.subplots()
-    counts = data["register"].value_counts()
-    names = counts.index
-    values = counts.values
-    counter = list(range(len(names)))
-
-    ax.bar(counter, values)
-    fig.show()
-
-
 def main():
     data_path = os.path.join(os.getcwd(), "data")
     seu_data, golden_data = DataReader.get_data(data_path)
-
-    plot_register_distribution(seu_data)
 
 
 if __name__ == "__main__":
