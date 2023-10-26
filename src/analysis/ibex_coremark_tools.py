b215577c010ae7c5e26746ce5137eccd791aec68
Anders Lauridsen <ahll19@student.aau.dk>
2023-10-26 10:33:13 +0200

    Added docstrings to all methods and created example config file

--- src/analysis/ibex_coremark_tools.py
+++ src/analysis/ibex_coremark_tools.py
@@ -12,6 +12,30 @@ class IbexCoremarkTools(BaseTools):
     def stacked_register_error_class(
         cls, seu_log: SeuLog, golden_log: pd.Series, visualize: bool = False
     ) -> Union[pd.DataFrame, Tuple[pd.DataFrame, plt.figure]]:
+        """
+        This method is only supposed to be called on the rf_regfile_i node-data of the
+        Coremark Ibex runs, but in theory it can be used on any node if you so wish.
+
+        This method takes all unique register names in the SeuLog (all ancestor nodes)
+        and computes the propoertion of errors on each one. Then it returns a dataframe
+        with these error propoertions.
+
+        The most usefel part of this method is visualizing and comparing the
+        distribution of errors visualize for all unique ancestors of a node (in the
+        intended case, each register of the core)
+
+        :param seu_log: SeuLog of a given run. Can be queried from the DataInterface
+        object, by usign a node.
+        :type seu_log: SeuLog
+        :param golden_log: Series showing the information parsed from the golden run.
+        can be found on the golden_log attribute of the DataInterface class.
+        :type golden_log: pd.Series
+        :param visualize: Whether or not to visalize the results, defaults to False
+        :type visualize: bool, optional
+        :return: The dataframe with the error rates for each ancestor of the node-data.
+        If visualize==True also returns the figure object from the visualization.
+        :rtype: Union[pd.DataFrame, Tuple[pd.DataFrame, plt.figure]]
+        """
         logs = {
             name: seu_log[seu_log["register"] == name]
             for name in seu_log["register"].unique()
